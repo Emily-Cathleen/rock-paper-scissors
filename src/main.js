2c33fc70497e28a038593097ba7cf7d8d8a36384
Emily Corcoran <cathleencorcoran@gmail.com>
2021-11-12 19:31:13 -0500

    Change innerHTML for choose fighter

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 var classicGame = document.querySelector(".classic-game");
 var difficultGame = document.querySelector('.difficult-game');
-
+var centerChoose = document.querySelector('.center-choose');
 
 function displayClassicGame() {
-  classicGame.classList.add('hidden');
-  difficultGame.classList.add('hidden');
+  hideMain();
 
 }
 
 function displayDifficultGame() {
-  classicGame.classList.add('hidden');
+  hideMain();
 }
 
+function hideMain() {
+  centerChoose.innerHTML = `<p class="center-choose">Choose your fighter!</p>`
+  classicGame.classList.add('hidden');
+  difficultGame.classList.add('hidden');
+
+}
 
 classicGame.addEventListener('click', displayClassicGame)
 difficultGame.addEventListener('click', displayDifficultGame)
