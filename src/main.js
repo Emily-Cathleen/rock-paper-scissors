8d022c6c7661b3b629e4f203890071e4e1629999
Emily Corcoran <cathleencorcoran@gmail.com>
2021-11-12 20:11:52 -0500

    Create add/hide helper functions and refactor existing code

--- src/main.js
+++ src/main.js
@@ -1,22 +1,36 @@
 var classicGame = document.querySelector(".classic-game");
 var difficultGame = document.querySelector('.difficult-game');
 var centerChoose = document.querySelector('.center-choose');
+var gameChoiceView = document.querySelector('.game-choice-view');
+var chooseFighterClassic = document.querySelector('.choose-fighter-classic');
+var chooseFighterDifficult = document.querySelector('.choose-fighter-difficult');
+
+function addHidden(element) {
+  element.classList.add('hidden');
+}
+
+function removeHidden(element) {
+  element.classList.remove('hidden');
+}
 
 function displayClassicGame() {
   hideMain();
+  removeHidden(chooseFighterClassic);
 
 }
 
 function displayDifficultGame() {
   hideMain();
+  removeHidden(chooseFighterDifficult);
 }
+
 
 function hideMain() {
   centerChoose.innerHTML = `<p class="center-choose">Choose your fighter!</p>`
-  classicGame.classList.add('hidden');
-  difficultGame.classList.add('hidden');
-
+  addHidden(gameChoiceView);
 }
+
+
 
 classicGame.addEventListener('click', displayClassicGame)
 difficultGame.addEventListener('click', displayDifficultGame)
